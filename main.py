318e4faa20f99e8483ef59bb4c70a53dbeee63ed
EntangledLabs <yiclemence@gmail.com>
2021-10-01 13:20:55 -0700

    Removed faulty MySQL config

--- main.py
+++ main.py
@@ -99,15 +99,11 @@ apt.upgrade()
 ufw.enable()
 ufw.reset()
 
-time.sleep(WAIT_TIME)
-
 # Removes any pre-existing LAMP installation or LAMP components
 apt.remove('apache2')
 apt.remove('mysql-server')
 apt.remove('php*')
 apt.autoremove()
-
-time.sleep(WAIT_TIME)
 
 # Installs a full LAMP stack
 
@@ -115,16 +111,12 @@ time.sleep(WAIT_TIME)
 apt.add_source('ppa:ondrej/php')
 apt.update()
 
-time.sleep(WAIT_TIME)
-
 # Installing apache2
 apt.install('apache2 libapache2-mod-evasive libapache2-mod-security2')
 ufw.add(80)
 ufw.add(443)
 systemctl.start('apache2')
 systemctl.enable('apache2')
-
-time.sleep(WAIT_TIME)
 
 mod_evasive = [
     '# $TFCName Script Entry - Apache2 ModEvasive Configuration $LogTime\n',
@@ -148,16 +140,12 @@ subprocess.call('a2enmod security2', shell=True)
 subprocess.call('a2enmod evasive', shell=True)
 systemctl.restart('apache2')
 
-time.sleep(WAIT_TIME)
-
 chmod.chmod_rec('/etc/apache2/conf/', 0o750)
 os.chmod('/usr/sbin/apache2', 0o511)
 os.chmod('/var/log/apache2', 0o750)
 chmod.chmod_rec('/etc/apache2/conf-available/', 0o640)
 chmod.chmod_rec('/etc/apache2/conf-enabled/', 0o640)
 os.chmod('/etc/apache2/apache2.conf', 0o640)
-
-time.sleep(WAIT_TIME)
 
 for dirpath, dirnames, filenames in os.walk('/var/www/html/'):
     shutil.chown(dirpath, 'www-data', 'www-data')
@@ -177,48 +165,14 @@ f.writelines(apache_lines)
 f.close()
 shutil.copy('newserver.conf', '/etc/apache2/conf-available/')
 
-time.sleep(WAIT_TIME)
-
 subprocess.call('a2enconf newserver', shell=True)
 subprocess.call('a2dissite 000-default', shell=True)
 systemctl.reload('apache2')
-
-time.sleep(WAIT_TIME)
 
 # Installing MySQL server
 apt.install('mysql-server')
 systemctl.start('mysql')
 systemctl.enable('mysql')
-
-
-time.sleep(WAIT_TIME)
-
-f = open('/var/lib/mysql/ubuntu.pid', 'r')
-mysqlpid = int(f.readline().strip('\n'))
-f.close()
-os.kill(mysqlpid, signal.SIGKILL)
-f = open('mysql_init', 'w+')
-f.write('ALTER USER \'root\'@\'localhost\' IDENTIFIED BY \'{}\''.format(getpass('Enter new MySQL root password: ')))
-f.close()
-shutil.chown('mysql_init', 'mysql')
-#subprocess.call('/bin/mysqld --init-file={}/mysql_init &'.format(os.getcwd()))
-Popen(['mysqld', '--init-file={}/mysql_init'.format(os.getcwd()), '&'])
-db = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password=getpass('Enter MySQL root password: ')
-)
-db_cursor = db.cursor()
-db_cursor.execute('DELETE FROM mysql.user WHERE User=\'\'')
-db_cursor.execute('DELETE FROM mysql.user WHERE User=\'root\' AND Host NOT IN (\'localhost\', \'127.0.0.1\', \'::1\')')
-db_cursor.execute('DROP DATABASE test')
-db_cursor.execute('DELETE FROM mysql.db WHERE Db=\'test\' OR Db=\'test\\_%\'')
-db_cursor.execute('FLUSH PRIVILEGES')
-db.close()
-systemctl.stop('mysql')
-systemctl.start('mysql')
-
-time.sleep(WAIT_TIME)
 
 # Installing PHP 8.0
 apt.install('php8.0 libapache2-mod-php8.0 php8.0-mysql php-common php8.0-cli php8.0-common php8.0-opcache php8.0-readline')
